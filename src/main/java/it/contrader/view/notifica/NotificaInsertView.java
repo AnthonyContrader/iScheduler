7ca3bbed0e385d1014b23bfd888bf144e1cbe0a2
AtiruNest <delben.gerri@gmail.com>
2019-10-10 10:32:52 +0200

    update insertviewnotifica

--- src/main/java/it/contrader/view/notifica/NotificaInsertView.java
+++ src/main/java/it/contrader/view/notifica/NotificaInsertView.java
@@ -1,5 +1,57 @@
 package it.contrader.view.notifica;
 
-public class NotificaInsertView {
+import it.contrader.controller.Request;
+import it.contrader.main.MainDispatcher;
+import it.contrader.view.AbstractView;
+
+public class NotificaInsertView extends AbstractView{
+	private Request request;
+
+	private boolean notificato;
+	private String notifica_tempo;
+	private int id_event;
+	private final String mode = "INSERT";
+
+	public NotificaInsertView() {
+	}
+	
+	/**
+	 * Se la request non è nulla (ovvero se si arriva dalla mode INSERT del controller) mostra
+	 * l'esito dell'operazione
+	 */
+	@Override
+	public void showResults(Request request) {
+		if (request!=null) {
+			System.out.println("Inserimento andato a buon fine.\n");
+			MainDispatcher.getInstance().callView("Notifica", null);
+		}
+	}
+
+	/**
+	 * Chiede all'utente di inserire gli attributi della notifica da inserire
+	 */
+	@Override
+	public void showOptions() {
+			System.out.println("Inserisci notificato della notifica:");
+			notificato = Boolean.parseBoolean(getInput().toString());
+			System.out.println("Inserisci notifica_tempo della notifica:");
+			notifica_tempo = getInput();
+			System.out.println("Inserisci id_event della notifica:");
+			id_event = Integer.parseInt(getInput().toString());
+	}
+
+	/**
+	 * Impacchetta la request con i dati inseriti nel metodo showOption()
+	 */
+	@Override
+	public void submit() {
+		request = new Request();
+		request.put("notificato", notificato);
+		request.put("notifica_tempo", notifica_tempo);
+		request.put("id_event", id_event);
+		request.put("mode", mode);
+		MainDispatcher.getInstance().callAction("Notifica", "doControl", request);
+	}
+
 
 }
