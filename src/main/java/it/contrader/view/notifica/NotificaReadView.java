83af5f3abba78946da2494c415bfd6c596cabc95
vimar96 <vittoriomarano96@gmail.com>
2019-10-10 11:02:10 +0200

    updte NotificaReadView

---
+++ src/main/java/it/contrader/view/notifica/NotificaReadView.java
@@ -0,0 +1,38 @@
+package it.contrader.view.notifica;
+
+import it.contrader.controller.Request;
+import it.contrader.dto.NotificaDTO;
+import it.contrader.main.MainDispatcher;
+import it.contrader.view.AbstractView;
+
+public class NotificaReadView extends AbstractView {
+	private Request request;
+	
+	private int id;
+	private final String mode = "READ";
+	
+	public NotificaReadView() {
+		
+	}
+
+	public void showResults(Request request) {
+		if (request != null) {
+			NotificaDTO notifica = (NotificaDTO) request.get("notifica");
+			System.out.println(notifica);
+			MainDispatcher.getInstance().callView("Notifica", null);
+		}
+	}
+	
+	public void showOptions() {
+		System.out.println("Inserisci id notifica che vuoi leggere");
+		id = Integer.parseInt(getInput());
+	}
+	
+	public void submit() {
+		request = new Request();
+		request.put("id", id);
+		request.put("mode", mode);
+		MainDispatcher.getInstance().callAction("Notifica","doControl",request);
+	}
+	
+}
