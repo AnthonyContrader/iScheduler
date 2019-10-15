7f227aacdfe8c77af08b65cf476fb479fb9e5731
unknown <rutiglianodev@gmail.com>
2019-10-15 12:02:55 +0200

    update DAO

--- src/main/java/it/contrader/converter/UserNotificationConverter.java
+++ src/main/java/it/contrader/converter/UserNotificationConverter.java
@@ -1,5 +1,36 @@
 package it.contrader.converter;
 
-public class UserNotificationConverter {
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.dto.UserNotificationDTO;
+import it.contrader.model.UserNotification;
+
+public class UserNotificationConverter implements Converter<UserNotification, UserNotificationDTO>  {
+	
+	public UserNotificationDTO toDTO(UserNotification userNotification) {
+		UserNotificationDTO userNotificationDTO = new UserNotificationDTO(userNotification.getId(), userNotification.getIdUser(), userNotification.getIdNotification());
+		return userNotificationDTO;
+		}
+	
+	
+	public UserNotification toEntity(UserNotificationDTO userNotificationDTO) {
+		UserNotification userNotification = new UserNotification(userNotificationDTO.getId(), userNotificationDTO.getIdUser(), userNotificationDTO.getIdNotification());
+		return userNotification;
+	}
+	
+	
+	public List<UserNotificationDTO> toDTOList(List<UserNotification> userNotificationList) {
+		//Crea una lista vuota.
+		List<UserNotificationDTO> userNotificationDTOList = new ArrayList<UserNotificationDTO>();
+		
+		//Cicla tutti gli elementi della lista e li converte uno a uno
+		for(UserNotification userNotification : userNotificationList) {
+			//Utilizza il metodo toDTO per convertire ogni singolo elemento della lista
+			//e lo aggiunge adda lista di DTO
+			userNotificationDTOList.add(toDTO(userNotification));
+		}
+		return userNotificationDTOList;
+	}
 
 }
---
+++ src/main/java/it/contrader/dao/UserNotificationDAO.java
@@ -0,0 +1,5 @@
+package it.contrader.dao;
+
+public class UserNotificationDAO {
+
+}
